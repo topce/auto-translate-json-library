6231d740c0b0bbfd1eb5a90d0cd16ad418546ee5
topce <topce@users.noreply.github.com>
2024-12-26 09:48:16 +0100

    update version

--- src/deepl.ts
+++ src/deepl.ts
@@ -1,74 +1,75 @@
-import type { ITranslate } from './translate.interface';
+import type { ITranslate } from "./translate.interface";
 
-import * as deepl from 'deepl-node';
+import * as deepl from "deepl-node";
 
-import { Util } from './util';
+import { Util } from "./util";
 
 const supportedLanguages = [
-  'AR',
-  'BG',
-  'CS',
-  'DA',
-  'DE',
-  'EL',
-  'EN',
-  'EN-GB',
-  'EN-US',
-  'EN',
-  'ES',
-  'ET',
-  'FI',
-  'FR',
-  'HU',
-  'ID',
-  'IT',
-  'JA',
-  'LT',
-  'LV',
-  'NB',
-  'NL',
-  'PL',
-  'PT',
-  'PT-PT',
-  'PT-BR',
-  'PT',
-  'RO',
-  'RU',
-  'SK',
-  'SL',
-  'SV',
-  'TR',
-  'UK',
-  'ZH',
+  "AR",
+  "BG",
+  "CS",
+  "DA",
+  "DE",
+  "EL",
+  "EN",
+  "EN-GB",
+  "EN-US",
+  "EN",
+  "ES",
+  "ET",
+  "FI",
+  "FR",
+  "HU",
+  "ID",
+  "IT",
+  "JA",
+  "LT",
+  "LV",
+  "NB",
+  "NL",
+  "PL",
+  "PT",
+  "PT-PT",
+  "PT-BR",
+  "PT",
+  "RO",
+  "RU",
+  "SK",
+  "SL",
+  "SV",
+  "TR",
+  "UK",
+  "ZH",
 ];
 
 export class DeepLTranslate implements ITranslate {
-  private endpoint = 'https://api.deepl.com';
-  constructor(private subscriptionKey: string, private type: 'free' | 'pro') {
-    if (this.type === 'free') {
-      this.endpoint = 'https://api-free.deepl.com';
+  private endpoint = "https://api.deepl.com";
+  constructor(
+    private subscriptionKey: string,
+    private type: "free" | "pro",
+  ) {
+    if (this.type === "free") {
+      this.endpoint = "https://api-free.deepl.com";
     }
   }
   isValidLocale(targetLocale: string): boolean {
-    console.log('targetLocale', targetLocale);
-    console.log('isValidLocale',  supportedLanguages.includes(targetLocale.toUpperCase()));
     return supportedLanguages.includes(targetLocale.toUpperCase());
   }
   async translateText(
     text: string,
     sourceLocale: string,
-    targetLocale: string
+    targetLocale: string,
   ): Promise<string> {
     let args: RegExpMatchArray | null;
     ({ args, text } = Util.replaceContextVariables(text));
 
-    let result = '';
+    let result = "";
 
     const translator = new deepl.Translator(this.subscriptionKey);
     const translation = await translator.translateText(
       text,
       sourceLocale as deepl.SourceLanguageCode,
-      targetLocale as deepl.TargetLanguageCode
+      targetLocale as deepl.TargetLanguageCode,
     );
 
     result = translation.text;
